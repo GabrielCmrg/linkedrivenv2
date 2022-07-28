bf7e1feccaa72f3af0a770a789dce6074586d008
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-07-28 12:32:41 -0300

    ex2

--- main.sql
+++ main.sql
@@ -1,3 +1,11 @@
 -- EXERCÍCIO 1
 
 SELECT COUNT("endDate") AS "currentExperiences" FROM experiences;
+
+-- EXERCÍCIO 2
+
+SELECT users.id, COUNT(educations.id) AS educations
+FROM educations
+JOIN users
+ON users.id = educations."userId"
+GROUP BY users.id;
